f7469eb3599118101f95dbc7f8c6d03b1965c388
emilechabal <98236600+emilechabal@users.noreply.github.com>
2022-01-27 22:09:42 +0000

    talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,12 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
-  [
     "London, UK",
     51.5073219,
-    -0.1276473
+    -0.1276474
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Rio de Janeiro, Brazil",
+    -22.9110137,
+    -43.2093727
   ]
 ];
