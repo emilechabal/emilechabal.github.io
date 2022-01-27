f448741cb3619c6eeb7eb223f317cc0f012710c5
emilechabal <98236600+emilechabal@users.noreply.github.com>
2022-01-27 22:32:54 +0000

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,17 +1,32 @@
 var addressPoints = [
   [
-    "London, UK",
+    "Institute of Historical Research, London, UK",
     51.5073219,
     -0.1276474
   ],
   [
-    "New York, NY, USA",
+    "Columbia University, New York, NY, USA",
     40.7127281,
     -74.0060152
-  ]
+  ],
   [
-  "Curitiba, Brazil"
-  -25.4295963
-  -49.2712724
-  ]
+    "Universidad Federal do Parana, Curitiba, Brazil"
+    -25.4295963
+    -49.2712724
+  ],
+  [
+    "CIDE, Mexico City, Mexico"
+    19.3748688
+    -99.2626256
+  ],
+  [
+    "Universidad Nacional de San Martin, Buenos Aires, Argentina"
+    -25.4295963
+    -49.2712724
+  ],
+  [
+  ""
+	-25.4295963
+	-49.2712724
+  ],
 ];
